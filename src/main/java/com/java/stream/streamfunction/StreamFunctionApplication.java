c59c4f63584f41f7594119c83f3ecd6bed81b540
Sharad Rai <sharad@linux-qhqj.attlocal.net>
2020-08-13 07:05:50 -0500

    intial check-in for functional programming

---
+++ src/main/java/com/java/stream/streamfunction/StreamFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.java.stream.streamfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamFunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/stream/streamfunction/StreamFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.stream.streamfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
